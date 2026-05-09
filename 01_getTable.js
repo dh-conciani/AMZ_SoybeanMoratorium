9855f58cf1609c63db006c8649fc4161c433b847
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2026-05-09 16:27:26 -0300

    Create 01_getTable.js

---
+++ 01_getTable.js
@@ -0,0 +1,123 @@
+// dhemerson.costa@ipam.org.br
+// get soybean per year
+
+// read mapbiomas 
+var mapbiomas = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10_1/mapbiomas_brazil_collection10_1_coverage_v1')
+  .eq(39)  // get only soybean
+  .selfMask()
+  .aside(print)
+
+Map.addLayer(mapbiomas.select('classification_2024'))
+
+
+
+var biomes = ee.Image('projects/mapbiomas-workspace/AUXILIAR/biomas-2019-raster');
+var states = ee.Image('projects/mapbiomas-workspace/AUXILIAR/estados-2016-raster');
+
+// read moratoria Prodes mask
+var prodes_mask = ee.Image('users/dh-conciani/help/rafa/Prodes_AmazLegal_1997a2025');
+Map.addLayer(prodes_mask.randomVisualizer(), {}, 'PRODES MASK')
+
+// prepare image
+var years = [ 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995,
+              1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
+              2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017,
+              2018, 2019, 2020, 2021, 2022, 2023, 2024];
+
+var recipe = ee.Image([]);
+years.forEach(function(year_i) {
+  
+  var result = biomes.multiply(100).add(states)
+                 .rename('classification_' + year_i)
+                 .updateMask(mapbiomas.select('classification_' + year_i))
+  
+  recipe = recipe.addBands(result)
+  
+})
+
+print(recipe)
+Map.addLayer(recipe.select('classification_2024'), {}, 'final')
+
+
+//////////////////
+// export areas
+// read territories
+var territory = prodes_mask.rename('territory')
+
+// change the scale if you need.
+var scale = 30;
+
+// define a Google Drive output folder 
+var driverFolder = 'SOY-MORATORIUM-RAFA';
+
+// get the classification for the file[i] 
+var asset_i = recipe
+
+// Image area in hectares
+var pixelArea = ee.Image.pixelArea().divide(10000);
+
+// Geometry to export
+var geometry = asset_i.geometry();
+
+// convert a complex object to a simple feature collection 
+var convert2table = function (obj) {
+  obj = ee.Dictionary(obj);
+    var territory = obj.get('territory');
+    var classesAndAreas = ee.List(obj.get('groups'));
+    
+    var tableRows = classesAndAreas.map(
+        function (classAndArea) {
+            classAndArea = ee.Dictionary(classAndArea);
+            var classId = classAndArea.get('class');
+            var area = classAndArea.get('sum');
+            var tableColumns = ee.Feature(null)
+                .set('territory', territory)
+                .set('class_id', classId)
+                .set('area', area);
+                
+            return tableColumns;
+        }
+    );
+  
+    return ee.FeatureCollection(ee.List(tableRows));
+};
+
+// compute the area
+var calculateArea = function (image, territory, geometry) {
+    var territotiesData = pixelArea.addBands(territory).addBands(image)
+        .reduceRegion({
+            reducer: ee.Reducer.sum().group(1, 'class').group(1, 'territory'),
+            geometry: geometry,
+            scale: scale,
+            maxPixels: 1e13
+        });
+        
+    territotiesData = ee.List(territotiesData.get('groups'));
+    var areas = territotiesData.map(convert2table);
+    areas = ee.FeatureCollection(areas).flatten();
+    return areas;
+};
+
+// perform per year 
+var areas = years.map(
+    function (year) {
+        var image = asset_i.select('classification_' + year);
+        var areas = calculateArea(image, territory, geometry);
+        // set additional properties
+        areas = areas.map(
+            function (feature) {
+                return feature.set('year', year);
+            }
+        );
+        return areas;
+    }
+);
+
+areas = ee.FeatureCollection(areas).flatten();
+  
+Export.table.toDrive({
+    collection: areas,
+    description: 'collection101-biomes-state-prodesMask-v3',
+    folder: driverFolder,
+    fileFormat: 'CSV'
+});
